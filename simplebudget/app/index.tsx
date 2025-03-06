d5e2500ab13f003a1e73edda8887cd7ee2334a7b
sujan2003 <niroulasujan7@gmail.com>
2025-03-06 16:44:28 -0600

    merge clean up

--- simplebudget/app/index.tsx
+++ simplebudget/app/index.tsx
@@ -23,23 +23,5 @@ export default function Index() {
 
       </SafeAreaView>
     </SafeAreaProvider>
-=======
-import { Text } from "react-native";
-import { SafeAreaView } from 'react-native-safe-area-context';
-import ItemBox from "./ItemBox";
-
-
-export default function Index() {
-  return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello World!</Text>
-      <ItemBox />
-    </SafeAreaView>
   );
 }
